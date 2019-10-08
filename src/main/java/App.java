cdfe98606e8c2302d14191faea0f07996f566324
melissa kobia <melissakobia1997@gmail.com>
2019-10-08 11:59:31 +0300

    Another small change

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,7 +19,7 @@ public class App {
         catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println("Hello");
+        System.out.println("Hello This is done");
 
     }
 
